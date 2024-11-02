05615d6050c5410a42fdb7f67c650908333e9793
FedeB <108481507+SirJimbus@users.noreply.github.com>
2024-11-02 12:15:06 +0100

    packages.

---
+++ utilities.ts
@@ -0,0 +1,20 @@
+// Configura qui il server locale e quello remoto
+// const LOCAL_SERVER_URL = "http://192.168.1.10:3000";
+// const REMOTE_SERVER_URL = "https://costvo-426021.ew.r.appspot.com";
+
+// // Scegli quale server usare commentando una delle due righe seguenti
+// //const CURRENT_SERVER_URL = LOCAL_SERVER_URL;
+// const CURRENT_SERVER_URL = REMOTE_SERVER_URL;
+
+// const getConfig = async () => {
+//   try {
+//     const response = await fetch(`${CURRENT_SERVER_URL}/api/config`);
+//     const config = await response.json();
+//     return config.serverUrl;
+//   } catch (error) {
+//     console.error("Failed to fetch config:", error);
+//     return "https://default-url.com"; // fallback
+//   }
+// };
+
+// export { getConfig, CURRENT_SERVER_URL };
